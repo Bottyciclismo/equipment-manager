e0a785c22471899c02e17d474e3488a8e586ab5b
botty <juancbotella@gmail.com>
2026-01-26 20:26:20 +0100

    Update AuthContext.jsx

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState, useEffect } from 'react';
-import { authAPI } from '../api/axios'; // Asegúrate que apunte a tu api.js o axios.js
+import { authAPI } from '../api/api'; // Asegúrate que apunte a tu api.js o axios.js
 
 const AuthContext = createContext();
 
